c2c6a5a244e8e41aa5cfdbfd92fc2e52af80b16d
Tomas Hermanek <hermanekt@users.noreply.github.com>
2018-05-30 14:39:15 +0200

    Create zabbix_get_jmx.sh

---
+++ zabbix_get_jmx.sh
@@ -0,0 +1,1 @@
+/usr/bin/zabbix_get_jmx --java-gateway-host JAVA_GATEWAY_HOST --java-gateway-port 10052 --jmx-server MONITORED_HOST --jmx-port 4447 --jmx-user USER --jmx-pass PASSWORD --new-protocol --key $1
